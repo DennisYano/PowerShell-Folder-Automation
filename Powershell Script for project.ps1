71c7bb7b2a6bb41f52cf7f92d37ee8870835ef2a
DennisYano <yanovskidennis@gmail.com>
2025-11-10 21:54:38 -0500

    Add files via upload

---
+++ Powershell Script for project.ps1
@@ -0,0 +1,12 @@
+﻿$intFolders = 10
+$intPad = 0
+$i = 1
+New-Variable -Name strPrefix -Value "testfolder" -Option Constant
+Do {
+if ($i -lt 10) {
+New-Item -path C:\ -Name "$strPrefix$intPad$i" -type directory
+} else {
+New-Item -path C:\ -Name "$strPrefix$i" -type directory
+}
+$i++
+} until ($i -eq $intFolders + 1)
